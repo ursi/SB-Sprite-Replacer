5b136d8946c3581830fb7666f14c7dad3de0dc31
Mason Mackaman <masondeanm@aol.com>
2022-02-05 23:01:29 -0500

    Refactor

--- flake.nix
+++ flake.nix
@@ -1,47 +1,44 @@
 { inputs =
-    { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-      utils.url = "github:ursi/flake-utils/2";
+    { make-shell.url = "github:ursi/nix-make-shell/1";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      utils.url = "github:ursi/flake-utils/8";
     };
 
   outputs = { utils, ... }@inputs:
     with builtins;
-    utils.default-systems
+    utils.apply-systems { inherit inputs; }
       ({ make-shell, pkgs, ... }:
-         let p = pkgs; l = p.lib; in
+         let l = p.lib; p = pkgs; in
          { packages.images =
              let
                images =
                  l.mapAttrs
                    (image: sha256:
                       fetchurl
-                        { url =
+                        { inherit sha256;
+
+                          url =
                             "https://ee.wrya.net/sprites/prod/VR1574197275-1.14.6/sheets/"
                             + image;
-                          inherit sha256;
                         }
                    )
                    (import ./images.nix);
              in
              p.runCommand "images" {}
                ''
-               mkdir $out
+               mkdir $out; cd $out
 
                ${toString
-                   ((l.mapAttrsToList
-                       (name: image: "ln -s ${image} $out/${name};")
-                    )
-                    images
+                   (l.mapAttrsToList
+                      (name: image: "ln -s ${image} ${name};")
+                      images
                    )
                }
                '';
 
            devShell =
              make-shell
-               { packages =
-                   with pkgs;
-                   [ (python3.withPackages (pp: [ pp.pillow ]))
-                   ];
-
+               { packages = [ (p.python3.withPackages (pp: [ pp.pillow ])) ];
                  aliases.build-sheets = "python sprite_replacer.py";
 
                  setup =
@@ -57,6 +54,5 @@
                };
 
          }
-      )
-      inputs;
+      );
 }
--- images.nix
+++ images.nix
@@ -1,137 +1,69 @@
-{ "apep.0.w5zg7pp_MNSd3iy7aJVBnA.png" =
-    "1zsy5b46cvm9wml062law2h7acbkzbmcfxxqb66hrz5jv7q0iqlz";
-  "apep.1.8F6E-msxKfUYwgh-QkBBqg.png" =
-    "12k3x984hjkx9imq8djvbqn4a9dnjr4bpbchxjhilj7qcm9r70z5";
-  "base.0.qL0rBMlFn6ac60uUrvSc0w.png" =
-    "0hl6b4prpd386h4pfbj0dph9kf1h95w8b4sh4vhn6q1vm77a2l30";
-  "base.1.j_z_6z4bG7QN93eXF12ccw.png" =
-    "0m09hzwxbxqy2rc4lmi1skvq6vwhyapwax56h20cf6152agfg4xv";
-  "bes.0.bbCWpat6eFNyLESl2gkvPA.png" =
-    "06s5p2ckwdhg7ksl7sfpl93cw2m7dd9s41qvpf6qf19x6ss4ds4g";
-  "cave-s.0.oGvXfujdMFa9WWNrAExpuw.png" =
-    "0h5pv8c8zj8n8h520qwm0330pbydlxkf99384ym4dgshw3ha0ssj";
-  "cave-s.1.yzZuMMHMmw2yrVv5O9pVKQ.png" =
-    "00c0gf7caasxipdf8ljjfsimmwm9hgcvjxfvlbc28im3raax2fc4";
-  "cave.0.jQ16KOfzXSMMr5Y0-o1IGg.png" =
-    "1n1n85xpdys6lhx4yc8y3c06x02578l6gdzg8rmpgb1p5kgcp8aj";
-  "cave.1.C0QTOviwmqHpdp1yJOiEBA.png" =
-    "09cg04p22r2lw6q6n0rgr0d67g7lqf7300awm7swcnsccvw88rzy";
-  "cave.2.RzGZ8Sm5RZtCOgyhBokLgA.png" =
-    "1nvgvji6yi1ksx0m8ca4iqk2f4pm84q9hipqn77k06sfdc2gl8iv";
-  "city-s.0.NC4oIInGJntuRVvBISfCkQ.png" =
-    "0dnvq13rnhi7l5ziiqz2w1gm7hfbhrwjji9wysjga8avfpf38zma";
-  "city.0.6pUZ96hBX_rQ15QstaHIAw.png" =
-    "0jg289igbalmyw9fhfs4965iw0q2264zc7g7fsqs3l7ws0x12c96";
-  "city.1.hGRwKZRYU3BvcwTAINrvow.png" =
-    "158dix05xh5s2nj2a7iigcb3zvvbz5qwc9snyld4mc3aww1cq3bk";
-  "city.2.nbaiKBxk9SX0HSsx0RTnKg.png" =
-    "19xv7pyiparng2rl5li3skmg6n7086kwrnvyj1z7rsdpm1nxp584";
-  "dumb-s.0.l7SwLelL2HK9r5NEHg4tBA.png" =
-    "17ks95ld9xxcllz6978z42dr97m10x72mljbjg5czs6aaj00cd4g";
-  "dumb.0.lot267eCUEPdINxFo4HdFA.png" =
-    "0mlb38wp4hmqrvi9kn674mw3dmaz0ngpzzlkp961lipvjfyqs34j";
-  "dumb.1.b3sMntApvZQQ5Ta60h4rnQ.png" =
-    "0v3cjq53r875026j9zsvmyh6cq96dqwmp24hm73q405jrmpsdb44";
-  "dumb.2.tlBYMNxfzpBBTgeSDJZN2Q.png" =
-    "0zfs887x5v7d6fkv6rg6pibbh5iy6hy4ygafhl1q78gasnxs2yqy";
-  "flappy.0.eJfFYgOB1thF0sHeAwrTQg.png" =
-    "15fnsa618q7l610hy6px222h9s59vd60ar6knvd96wb2j22gakd4";
-  "forest-s.0.mj4VHB7FqB_uq7GWZW6SNg.png" =
-    "1fajlqh4gn5h4fj1sy003zl6lc0zd8nfsjaxwyfbnvmbnjxx89xi";
-  "forest-s.1.Gvu_Q7vPJT-t1ujxCAXUaQ.png" =
-    "1dxf5ccbfcywdbjlaajwjmf009amhn58998qf21hwx288q6v7gwf";
-  "forest.0.ChlHgwfY_yBRVkkzOoYMjA.png" =
-    "069w42jlpm9wwwnr109xargql7h62pz8k44fzkffqxarr9ysczi3";
-  "forest.1.44MsrhO6JYCuPQidD49qjg.png" =
-    "0mja4fjg7zlybz4zd5l6szs8dwfppcn8nf41sng50ni6s1s7mblg";
-  "forest.2.--E5AiKYptM0fx7MvYusSA.png" =
-    "14bafni7byq1826n7v19dlrv2zcgvjp5vm09zyaqrcqpyc189zz0";
-  "forest.3.LaWca5TdvANp7tZBSlHiPg.png" =
-    "13g19mnjgyi2bm4131r2d99cp6lmyx40c1x6whrxzissyqvd69rf";
-  "home.0.Mc_Eky3aMxPoS233zZH7Uw.png" =
-    "01b01jr52s0rryqvjs2sj0n1l9ygz9v2iyv7w5rcki262asc2nj1";
-  "home.1.rho5n2X4mzj7dANgRtCNAA.png" =
-    "01pk8cqkzsf7pxm917w91lhn4bwcidg2gy3d78p9x7rrpr1nk4hl";
-  "horus.0.tQV-UWFmPVTxKmx0LHW1gw.png" =
-    "09vn13030cb8dccvzr31bn6zkmxlqr9ikjyw08fv2h9x7lgah4py";
-  "horus.1.beNhxO0iC_yBYF7NvQkWOA.png" =
-    "1406kyqafvm2p74whh9gc1hh026sdx16ybldp1ps831azgn3008g";
-  "horus.2.wl2wtZiELiq1UiwPLXS5Yg.png" =
-    "1y48xgiy2y1yvs0j6d3njvslp3gvrvdg8lyji1ykl68cazcfqfw3";
-  "hulk-s.0.2TNnj0HzbKtwn5az8kYBZQ.png" =
-    "0i2szzywkil4z8gl7x79yg8w4xzix7jm3rhx1sy72l3w4xs1dl0b";
-  "hulk-s.1.x7pAgUAIIY3ymoyvToMStg.png" =
-    "0an5gx1ibyab181w035krgxl5m728pv6fp7bw83s6fzmdi3jyi1g";
-  "hulk.0.pHTbK-Xql-phUd4ZQPfakw.png" =
-    "0vkb9y3l8gqca5vcvijcalnkqg6mv0xacar4qvj3jp47s68mh3nb";
-  "hulk.1.nfnRkZIcq50duR3y1mEubA.png" =
-    "0zhch903jg9hjs6x2cwkqc74rvwf81h3cv462mn1gp4mbcvpq0vj";
-  "hulk.2.7neYZn9tUilLq6ShYaw8IQ.png" =
-    "18715y423laa6yfvmzwjgycm7f75bch1d1pvjh11ccdrcsjjry76";
-  "ice.0.SsXo7zrdBpZIXscBZcZftQ.png" =
-    "1za310psp4ynmv1vq6dd58ixrdrlspc4aq6pjglyan0r721ya8mf";
-  "ice.1.O8R_q020FvT4lZDpg5hfMQ.png" =
-    "0h4qxmhwq2ni2s2c3na342hm16z97z1bjac150bm8nkiqym1a85c";
-  "ice.2.UFhFrZ54GolA29TUdBBmiA.png" =
-    "09wy5nkbdii5mqdz6nqg1ia9yrcqklnl7xrbglgffqcv6vzyhvrf";
-  "ice.3.Ez8pQmOEP408T5flZfnkaA.png" =
-    "1k4rb7qjll7wfxhyvs6fnncds21g314q8fr29xzjh91rniznp2dm";
-  "jungle-s.0.T_zlU4PcEeEkZe2wkMIq1w.png" =
-    "1yxykyyzh2sq1fpjzx0p1gh1l9rbq3svbayf70rlax1zw9xlhzcc";
-  "jungle.0.1AE36gSsZaxX_gaBcwGoKw.png" =
-    "1kwbr79pbc05k95i9dlhk7c9v0ryrdasmcgyhpv620sj5ifk9kws";
-  "jungle.1.Z5rTCxDj68nOav0rCTBA8w.png" =
-    "183arnspksp602cid925w2sdznsp6gfasilzb676pq1a7ravba41";
-  "jungle.2.zTj8xOcXQoZiVxxGwLfYJQ.png" =
-    "0qzd23gqn71rrjg318fgacac9ka8j5asf8lzf54b8m9axx4ack1k";
-  "lab-s.0.vFKr8Kbu4oJuIrJeRmmISw.png" =
-    "195hps1lm36y80g4kxddkc188ky4pqc1wcy2p4vbnyp14q6qma04";
-  "lab.0.pp4R0wG1qOIpJi7EAEUiXg.png" =
-    "1f3kx9d6pndyy5x8agnnfqhli40vlv6y8612l55397rpp8n8zvc4";
-  "lab.1.wKmzm_KjWts4P9FeoGRofw.png" =
-    "148wi00b2s3500g2hk5k3vdmc798s0ax4qj12dhknsrnxpb6ajc5";
-  "lab.2.15Kll_-3cOfLyh9zrzyJOw.png" =
-    "1bwi6m04ywlpld13dj9xbnv33qfpm536blpa6ny3shdwsk83zj2f";
-  "load.0.HdR5jgYV8czhe8PzZAH7hQ.png" =
-    "1qjrz10kcg75jmlmwr73k57zs8q4zlll99fvw5ggw2giy2028wfp";
-  "lobby.0.uYU8_bQDb4_kKYOp4WwEfA.png" =
-    "0bafx1x731i24g6b051ymiml8sdc7a5k2bgzpw7qa04sjnh6yxxi";
-  "lobby.1.OiuTJSxSQNLpVmbAjPB0MA.png" =
-    "185slmyjikiivx4vvw9fczpvxvv2rhcqfka6wgzklnl3xwd5aj5s";
-  "ptah.0.KOYJIqjrnv3UepUllOgj8w.png" =
-    "1li8l36c2487lbljq4k441vhxjgqkf4mmnc0gl581ica6y6v8yfr";
-  "sc.0.hCr8Gxo_aVB0O0wCwXLyCg.png" =
-    "087q9k4clw2mqwdnp4z7p3h6aiqvdbm258scx1rv8rc572gw9q3d";
-  "sc.1.EUKDtiMEWT9fhf7wRBn84Q.png" =
-    "1xs8liyzyfv2jwy9qd08hc5p0lrxgxnb0l5kmbd0x6ca3md3b28i";
-  "sc.2.6x4Ckm1He0MmRnf_wUnYiQ.png" =
-    "0iia3bhxkzrssg0h40k3brbw247p5k4mdmxd0whyp1az7kpkcphs";
-  "scboss.0.AF21vkG1kKv6QmXZQakjZg.png" =
-    "16bdphpmrpf97pljp7hpmjc2bjza66yf470hal1cc84argmr9rqr";
-  "scboss.1.zCuHdTQUWtQ323y0n-0l3g.png" =
-    "0a0i83s145p91gydrhz5akmsynrjn13c6nyh2dbbcx9308dwza9f";
-  "scboss.2.EftCYw57FArq3am_g0Ml8g.png" =
-    "0ryblm3z1acpxcgh7fmsn44al8ckcnnl6dh5j6yng0sm3pqqj7ip";
-  "set.0.0kmU5FwVyDitHbuY2kiPdQ.png" =
-    "007cvc2dx2dv3xkxs3p043a7wjngz13ampvzlgqmla9z5gcmq7rh";
-  "shu.0.wHtdL1ttmejRv4uqwUfmMQ.png" =
-    "084qxhhzdiixc9j4kwvdqpy94jim4wsylnxzd1h12ayl7cqd952z";
-  "test.0._KrqFCzRkjx_-EK7vfbvXQ.png" =
-    "07cigg9s5vnagx9lrhgihkvzq446nw39l2kynbjxy2iwsna498np";
-  "thoth.0.Unt4NhjO7eai1MJkO_o6CQ.png" =
-    "141l1jfn7c4s5kzs7yz1a2yj6fgy01xy7iwjq9cby09czdy0p87f";
-  "title.0.qwwVZtekzv-2pd5fYl1zPw.png" =
-    "0jz0gpi9l29cwf5zd78arhyz99j2d1rmbp4k2x39j4d6nqj8grwf";
-  "tutorial.0.KoTvkb3h8vP2TOe652XNVw.png" =
-    "0kw0xfrz0scxs0n79r4rw7jfplfdhyvjl3va7dqplns0m7bqirvv";
-  "tutorial.1.G5eFishICimeNX_PHaI4_A.png" =
-    "0nmq3y9bfs54d95sr5pcvcsldrf032v572li6x5gb3j6wx1ia7h6";
-  "tutorial.2.PkMzxYwI9b_YyZRWBsakqw.png" =
-    "1dy08ypi03fpl24234gvsm5vncb0ipic4jkkm3g63h51f089m2g5";
-  "tutorial.3.mPKh-0b_myd1H5ZefTns1w.png" =
-    "0zmnfcz9p455njw0iiyvbqm25lf8lgspw7rc6v42jjrxg5g8hfg3";
-  "ui.0.clnXQ21vegl24LiO9GpOyw.png" =
-    "1ax66279pgd16gkb7qqh52wxz2xrg6yj9pfjs518dsg63mynmkjq";
-  "wings.0.p7m3gmXJIBX9ZcegzeKPxw.png" =
-    "1zqsvsk28ngp85ya7gyxr2ql6sqpqk7szkkqq391a1x8piz6jvkw";
+{ "apep.0.w5zg7pp_MNSd3iy7aJVBnA.png" = "1zsy5b46cvm9wml062law2h7acbkzbmcfxxqb66hrz5jv7q0iqlz";
+  "apep.1.8F6E-msxKfUYwgh-QkBBqg.png" = "12k3x984hjkx9imq8djvbqn4a9dnjr4bpbchxjhilj7qcm9r70z5";
+  "base.0.qL0rBMlFn6ac60uUrvSc0w.png" = "0hl6b4prpd386h4pfbj0dph9kf1h95w8b4sh4vhn6q1vm77a2l30";
+  "base.1.j_z_6z4bG7QN93eXF12ccw.png" = "0m09hzwxbxqy2rc4lmi1skvq6vwhyapwax56h20cf6152agfg4xv";
+  "bes.0.bbCWpat6eFNyLESl2gkvPA.png" = "06s5p2ckwdhg7ksl7sfpl93cw2m7dd9s41qvpf6qf19x6ss4ds4g";
+  "cave-s.0.oGvXfujdMFa9WWNrAExpuw.png" = "0h5pv8c8zj8n8h520qwm0330pbydlxkf99384ym4dgshw3ha0ssj";
+  "cave-s.1.yzZuMMHMmw2yrVv5O9pVKQ.png" = "00c0gf7caasxipdf8ljjfsimmwm9hgcvjxfvlbc28im3raax2fc4";
+  "cave.0.jQ16KOfzXSMMr5Y0-o1IGg.png" = "1n1n85xpdys6lhx4yc8y3c06x02578l6gdzg8rmpgb1p5kgcp8aj";
+  "cave.1.C0QTOviwmqHpdp1yJOiEBA.png" = "09cg04p22r2lw6q6n0rgr0d67g7lqf7300awm7swcnsccvw88rzy";
+  "cave.2.RzGZ8Sm5RZtCOgyhBokLgA.png" = "1nvgvji6yi1ksx0m8ca4iqk2f4pm84q9hipqn77k06sfdc2gl8iv";
+  "city-s.0.NC4oIInGJntuRVvBISfCkQ.png" = "0dnvq13rnhi7l5ziiqz2w1gm7hfbhrwjji9wysjga8avfpf38zma";
+  "city.0.6pUZ96hBX_rQ15QstaHIAw.png" = "0jg289igbalmyw9fhfs4965iw0q2264zc7g7fsqs3l7ws0x12c96";
+  "city.1.hGRwKZRYU3BvcwTAINrvow.png" = "158dix05xh5s2nj2a7iigcb3zvvbz5qwc9snyld4mc3aww1cq3bk";
+  "city.2.nbaiKBxk9SX0HSsx0RTnKg.png" = "19xv7pyiparng2rl5li3skmg6n7086kwrnvyj1z7rsdpm1nxp584";
+  "dumb-s.0.l7SwLelL2HK9r5NEHg4tBA.png" = "17ks95ld9xxcllz6978z42dr97m10x72mljbjg5czs6aaj00cd4g";
+  "dumb.0.lot267eCUEPdINxFo4HdFA.png" = "0mlb38wp4hmqrvi9kn674mw3dmaz0ngpzzlkp961lipvjfyqs34j";
+  "dumb.1.b3sMntApvZQQ5Ta60h4rnQ.png" = "0v3cjq53r875026j9zsvmyh6cq96dqwmp24hm73q405jrmpsdb44";
+  "dumb.2.tlBYMNxfzpBBTgeSDJZN2Q.png" = "0zfs887x5v7d6fkv6rg6pibbh5iy6hy4ygafhl1q78gasnxs2yqy";
+  "flappy.0.eJfFYgOB1thF0sHeAwrTQg.png" = "15fnsa618q7l610hy6px222h9s59vd60ar6knvd96wb2j22gakd4";
+  "forest-s.0.mj4VHB7FqB_uq7GWZW6SNg.png" = "1fajlqh4gn5h4fj1sy003zl6lc0zd8nfsjaxwyfbnvmbnjxx89xi";
+  "forest-s.1.Gvu_Q7vPJT-t1ujxCAXUaQ.png" = "1dxf5ccbfcywdbjlaajwjmf009amhn58998qf21hwx288q6v7gwf";
+  "forest.0.ChlHgwfY_yBRVkkzOoYMjA.png" = "069w42jlpm9wwwnr109xargql7h62pz8k44fzkffqxarr9ysczi3";
+  "forest.1.44MsrhO6JYCuPQidD49qjg.png" = "0mja4fjg7zlybz4zd5l6szs8dwfppcn8nf41sng50ni6s1s7mblg";
+  "forest.2.--E5AiKYptM0fx7MvYusSA.png" = "14bafni7byq1826n7v19dlrv2zcgvjp5vm09zyaqrcqpyc189zz0";
+  "forest.3.LaWca5TdvANp7tZBSlHiPg.png" = "13g19mnjgyi2bm4131r2d99cp6lmyx40c1x6whrxzissyqvd69rf";
+  "home.0.Mc_Eky3aMxPoS233zZH7Uw.png" = "01b01jr52s0rryqvjs2sj0n1l9ygz9v2iyv7w5rcki262asc2nj1";
+  "home.1.rho5n2X4mzj7dANgRtCNAA.png" = "01pk8cqkzsf7pxm917w91lhn4bwcidg2gy3d78p9x7rrpr1nk4hl";
+  "horus.0.tQV-UWFmPVTxKmx0LHW1gw.png" = "09vn13030cb8dccvzr31bn6zkmxlqr9ikjyw08fv2h9x7lgah4py";
+  "horus.1.beNhxO0iC_yBYF7NvQkWOA.png" = "1406kyqafvm2p74whh9gc1hh026sdx16ybldp1ps831azgn3008g";
+  "horus.2.wl2wtZiELiq1UiwPLXS5Yg.png" = "1y48xgiy2y1yvs0j6d3njvslp3gvrvdg8lyji1ykl68cazcfqfw3";
+  "hulk-s.0.2TNnj0HzbKtwn5az8kYBZQ.png" = "0i2szzywkil4z8gl7x79yg8w4xzix7jm3rhx1sy72l3w4xs1dl0b";
+  "hulk-s.1.x7pAgUAIIY3ymoyvToMStg.png" = "0an5gx1ibyab181w035krgxl5m728pv6fp7bw83s6fzmdi3jyi1g";
+  "hulk.0.pHTbK-Xql-phUd4ZQPfakw.png" = "0vkb9y3l8gqca5vcvijcalnkqg6mv0xacar4qvj3jp47s68mh3nb";
+  "hulk.1.nfnRkZIcq50duR3y1mEubA.png" = "0zhch903jg9hjs6x2cwkqc74rvwf81h3cv462mn1gp4mbcvpq0vj";
+  "hulk.2.7neYZn9tUilLq6ShYaw8IQ.png" = "18715y423laa6yfvmzwjgycm7f75bch1d1pvjh11ccdrcsjjry76";
+  "ice.0.SsXo7zrdBpZIXscBZcZftQ.png" = "1za310psp4ynmv1vq6dd58ixrdrlspc4aq6pjglyan0r721ya8mf";
+  "ice.1.O8R_q020FvT4lZDpg5hfMQ.png" = "0h4qxmhwq2ni2s2c3na342hm16z97z1bjac150bm8nkiqym1a85c";
+  "ice.2.UFhFrZ54GolA29TUdBBmiA.png" = "09wy5nkbdii5mqdz6nqg1ia9yrcqklnl7xrbglgffqcv6vzyhvrf";
+  "ice.3.Ez8pQmOEP408T5flZfnkaA.png" = "1k4rb7qjll7wfxhyvs6fnncds21g314q8fr29xzjh91rniznp2dm";
+  "jungle-s.0.T_zlU4PcEeEkZe2wkMIq1w.png" = "1yxykyyzh2sq1fpjzx0p1gh1l9rbq3svbayf70rlax1zw9xlhzcc";
+  "jungle.0.1AE36gSsZaxX_gaBcwGoKw.png" = "1kwbr79pbc05k95i9dlhk7c9v0ryrdasmcgyhpv620sj5ifk9kws";
+  "jungle.1.Z5rTCxDj68nOav0rCTBA8w.png" = "183arnspksp602cid925w2sdznsp6gfasilzb676pq1a7ravba41";
+  "jungle.2.zTj8xOcXQoZiVxxGwLfYJQ.png" = "0qzd23gqn71rrjg318fgacac9ka8j5asf8lzf54b8m9axx4ack1k";
+  "lab-s.0.vFKr8Kbu4oJuIrJeRmmISw.png" = "195hps1lm36y80g4kxddkc188ky4pqc1wcy2p4vbnyp14q6qma04";
+  "lab.0.pp4R0wG1qOIpJi7EAEUiXg.png" = "1f3kx9d6pndyy5x8agnnfqhli40vlv6y8612l55397rpp8n8zvc4";
+  "lab.1.wKmzm_KjWts4P9FeoGRofw.png" = "148wi00b2s3500g2hk5k3vdmc798s0ax4qj12dhknsrnxpb6ajc5";
+  "lab.2.15Kll_-3cOfLyh9zrzyJOw.png" = "1bwi6m04ywlpld13dj9xbnv33qfpm536blpa6ny3shdwsk83zj2f";
+  "load.0.HdR5jgYV8czhe8PzZAH7hQ.png" = "1qjrz10kcg75jmlmwr73k57zs8q4zlll99fvw5ggw2giy2028wfp";
+  "lobby.0.uYU8_bQDb4_kKYOp4WwEfA.png" = "0bafx1x731i24g6b051ymiml8sdc7a5k2bgzpw7qa04sjnh6yxxi";
+  "lobby.1.OiuTJSxSQNLpVmbAjPB0MA.png" = "185slmyjikiivx4vvw9fczpvxvv2rhcqfka6wgzklnl3xwd5aj5s";
+  "ptah.0.KOYJIqjrnv3UepUllOgj8w.png" = "1li8l36c2487lbljq4k441vhxjgqkf4mmnc0gl581ica6y6v8yfr";
+  "sc.0.hCr8Gxo_aVB0O0wCwXLyCg.png" = "087q9k4clw2mqwdnp4z7p3h6aiqvdbm258scx1rv8rc572gw9q3d";
+  "sc.1.EUKDtiMEWT9fhf7wRBn84Q.png" = "1xs8liyzyfv2jwy9qd08hc5p0lrxgxnb0l5kmbd0x6ca3md3b28i";
+  "sc.2.6x4Ckm1He0MmRnf_wUnYiQ.png" = "0iia3bhxkzrssg0h40k3brbw247p5k4mdmxd0whyp1az7kpkcphs";
+  "scboss.0.AF21vkG1kKv6QmXZQakjZg.png" = "16bdphpmrpf97pljp7hpmjc2bjza66yf470hal1cc84argmr9rqr";
+  "scboss.1.zCuHdTQUWtQ323y0n-0l3g.png" = "0a0i83s145p91gydrhz5akmsynrjn13c6nyh2dbbcx9308dwza9f";
+  "scboss.2.EftCYw57FArq3am_g0Ml8g.png" = "0ryblm3z1acpxcgh7fmsn44al8ckcnnl6dh5j6yng0sm3pqqj7ip";
+  "set.0.0kmU5FwVyDitHbuY2kiPdQ.png" = "007cvc2dx2dv3xkxs3p043a7wjngz13ampvzlgqmla9z5gcmq7rh";
+  "shu.0.wHtdL1ttmejRv4uqwUfmMQ.png" = "084qxhhzdiixc9j4kwvdqpy94jim4wsylnxzd1h12ayl7cqd952z";
+  "test.0._KrqFCzRkjx_-EK7vfbvXQ.png" = "07cigg9s5vnagx9lrhgihkvzq446nw39l2kynbjxy2iwsna498np";
+  "thoth.0.Unt4NhjO7eai1MJkO_o6CQ.png" = "141l1jfn7c4s5kzs7yz1a2yj6fgy01xy7iwjq9cby09czdy0p87f";
+  "title.0.qwwVZtekzv-2pd5fYl1zPw.png" = "0jz0gpi9l29cwf5zd78arhyz99j2d1rmbp4k2x39j4d6nqj8grwf";
+  "tutorial.0.KoTvkb3h8vP2TOe652XNVw.png" = "0kw0xfrz0scxs0n79r4rw7jfplfdhyvjl3va7dqplns0m7bqirvv";
+  "tutorial.1.G5eFishICimeNX_PHaI4_A.png" = "0nmq3y9bfs54d95sr5pcvcsldrf032v572li6x5gb3j6wx1ia7h6";
+  "tutorial.2.PkMzxYwI9b_YyZRWBsakqw.png" = "1dy08ypi03fpl24234gvsm5vncb0ipic4jkkm3g63h51f089m2g5";
+  "tutorial.3.mPKh-0b_myd1H5ZefTns1w.png" = "0zmnfcz9p455njw0iiyvbqm25lf8lgspw7rc6v42jjrxg5g8hfg3";
+  "ui.0.clnXQ21vegl24LiO9GpOyw.png" = "1ax66279pgd16gkb7qqh52wxz2xrg6yj9pfjs518dsg63mynmkjq";
+  "wings.0.p7m3gmXJIBX9ZcegzeKPxw.png" = "1zqsvsk28ngp85ya7gyxr2ql6sqpqk7szkkqq391a1x8piz6jvkw";
 }
